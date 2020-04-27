0e2badcea3acb0d24ef4703aaf8e6312cb52fee7
James Batsell <jamesbatsell05@gmail.com>
2020-04-27 22:54:22 +0000

    Automatically backed up by Learn

--- db/migrate/20200427225045_create_users.rb
+++ db/migrate/20200427225045_create_users.rb
@@ -1,9 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
-    create_table do |t|
-      t.string :email
-      t.string :password_diggest
+    create_table :users do |t|
+      t.string :username
+      t.string :password_digest
     end
-      
   end
 end
